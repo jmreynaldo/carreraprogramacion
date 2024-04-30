d602e0c0d743d4506e31c3bbe0a8c81db54500a2
jmreynaldo <javier_reynaldo@yahoo.com.ar>
2024-04-30 02:27:52 -0300

    alumnos error solucionado

--- javier/alumnos.js
+++ javier/alumnos.js
@@ -3,60 +3,98 @@ var xdni= document.querySelector(".dni");
 var xmateria= document.querySelector(".materia");
 var xagregar= document.querySelector(".agregar");
 var xnota= document.querySelector(".nota");
-var xboton= document.querySelector(".notas");
-var tbalumnos= document.querySelector(".tablaNotas");
+var verNotas= document.querySelector(".verNotas");
+var tbNotas= document.querySelector(".tablaNotas");
+
 xagregar.addEventListener("click", function(){
+	
+	if (isNaN(xdni.value) || xdni.value==0) {
+		alert("Por favor ingrese un DNI");
+		return false;
+
+	}
+	
 	if(alumnos[xdni.value]=== undefined){
-		alert("El alumno no existe");
-		alumnos[xdni.value]={
+		//Agrego el alumno con datos en blanco
+		alumnos[xdni.value] = {
 			"Matematica": [],
 			"Programacion": [],
-			"Sistemas": [],
-			
-		}
-			alumnos[xdni.value][`${xmateria.value}`].push(parseInt(xnota.value));
-		console.log(alumnos)
-	}
-	else{
-			//el alumnos y la materia ya estan cargadas
-				alumnos[xdni.value][`${xmateria.value}`].push(parseInt(xnota.value));	
+			"Sistemas": []
 		}
 		
-
-console.log(xdni.value);
-console.log(xmateria.value);
+	}
+	if (xnota.value == 0) {
+		alert("Por favor ingrese la nota");
+		return false;
+	}
+	alumnos[xdni.value][`${xmateria.value}`].push(parseInt(xnota.value));	
 })
-xboton.addEventListener("click",function(){
-	tbalumnos.innerHTML="";
-		var mMateria1= alumnos[xdni.value]["Matematica"];
-		var P1= Promedio(mMateria1);
-		var mMateria2= alumnos[xdni.value]["Programacion"];
-		var mMateria3= alumnos[xdni.value]["Sistemas"];
-	tbalumnos.innerHTML+= `
+
+
+verNotas.addEventListener("click",function(){
+	
+	if (isNaN(xdni.value) || xdni.value==0) {
+		alert("Por favor ingrese un DNI");
+		return false;
+
+	}
+	if (alumnos[xdni.value]===undefined) {
+		alert("No existen notas para este DNI");
+		return false;
+
+	}
+	var mMateria1 = alumnos[xdni.value]["Matematica"];
+	var mMateria2 = alumnos[xdni.value]["Programacion"];
+	var mMateria3 = alumnos[xdni.value]["Sistemas"];
+	let p1 = xPromedio(mMateria1);
+	let p2 = xPromedio(mMateria2);
+	let p3 = xPromedio(mMateria3);
+
+	tbNotas.innerHTML="";
+	tbNotas.innerHTML+= `
 			<tr>
 				<td>Matematica</td>
 				<td>${mMateria1}</td>
-				<td>${P1}</td>
+				<td>${p1}</td>
+				<td>${siAprobo(p1)}</td>
 			</tr>
 			<tr>
 				<td>Programacion</td>
 				<td>${mMateria2}</td>
+				<td>${p2}</td>
+				<td>${siAprobo(p2)}</td>
 			</tr>
 			<tr>
 				<td>Sistemas</td>
 				<td>${mMateria3}</td>
+				<td>${p3}</td>
+				<td>${siAprobo(p3)}</td>
 			</tr>
 		`;
 })
 
-function Promedio(xN){
-	var Total= 0;
-	var Pro= 0; 
-	for (let i=0; xN.length-1; i++){
-		Total=Total+xN[i];
-
+function xPromedio(xN) {
+	var Total = 0;
+	var Prom = 0;
+	
+	if (xN.length <= 0) { 
+		return 0
+	}
+	
+	for (let i=0; i < xN.length; i++) {
+		Total=Total + xN[i];
 
 	}
-	Pro= Total/xN.length;
-	return Pro
+
+	Prom = (Total/xN.length).toFixed(2);
+	return Prom
+}
+function siAprobo(xp) {
+	if (xp >= 6) {
+		return "Aprobado   "
+	} 
+	if (xp>0 && xp<6) {
+		return "Desaprobado"
+	}
+	return "sin Calif."
 }
